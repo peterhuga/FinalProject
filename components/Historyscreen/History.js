25cd3eeee5f8afebc3c8ebfc08c82d06d5e8dfa6
Jianwei Wang <j_wang68715@fanshaweonline.ca>
2022-08-04 21:59:29 -0400

    Some history done1

--- components/Historyscreen/History.js
+++ components/Historyscreen/History.js
@@ -1,8 +1,45 @@
-import React from "react";
-import { View, StyleSheet, Text, Button, Snackbar,  ScrollView } from "react-native";
-
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Button,
+  Snackbar,
+  ScrollView,
+} from "react-native";
+import { dbMonthGroup, dbGetMonthly, dbGetUser } from "../../tools/sqlite";
 
 export default function History() {
+  console.log("History starts");
+  const [dailyRecord, setDailyRecord] = useState([]);
+
+  useEffect(() => {
+    dbGetUser()
+      .then((result) => {
+        const dbUser = result.rows._array;
+        // ScrollView display recordes for up to 20 days
+        if (dbUser.length > 20) {
+          setDailyRecord(dbUser.slice(-20, 0));
+          console.log("Max 20: ", dailyRecord);
+        } else {
+          setDailyRecord(dbUser);
+          console.log("History: ", dailyRecord);
+        }
+
+      })
+      .catch((error) => {
+        console.log("Get Error: ", error);
+      });
+  }, []);
+
+  // dbGetMonthly()
+  //   .then((r) => {
+  //     console.log("MonthGroup result: ", r);
+  //   })
+  //   .catch((error) => {
+  //     console.log("MonthGroup Error: ", error);
+  //   });
+
   return (
     <View>
       <View>
@@ -10,7 +47,7 @@ export default function History() {
       </View>
       <View>
         <ScrollView>
-            <Text>July</Text>
+          <Text>July</Text>
         </ScrollView>
       </View>
     </View>
