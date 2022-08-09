043bd192c9a3c8004bf9a77ae382013553ecd225
Jianwei Wang <j_wang68715@fanshaweonline.ca>
2022-08-09 15:55:06 -0400

    init

--- components/Homescreen/Weather.js
+++ components/Homescreen/Weather.js
@@ -5,44 +5,46 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 const apiKey = "c8eb38d3cf154612aec35303221707";
-const baseUrl =
-  "http://api.weatherapi.com/v1/forecast.json?key=c8eb38d3cf154612aec35303221707&q=London&aqi=no";
+const baseUrl = "http://api.weatherapi.com/v1/forecast.json?";
 
 export default function Weather() {
   console.log("Weather starts");
   const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
   const [temp, setTemp] = useState("");
   const [weather, setWeather] = useState("");
   const [iconImageUrl, setIconImageUrl] = useState("");
   var coords;
 
-  
   (async () => {
     try {
       coords = await getLocation();
       console.log("Coordinates: ", coords);
+      const lat = coords.latitude;
+      const lng = coords.longitude;
+      axios
+        .get(`${baseUrl}key=${apiKey}&q=${lat},${lng}&dyas=1&aqi=no&alerts=no`)
+        .then((r) => {
+          console.log(r.data);
+          setCity(r.data.location.name);
+          setCountry(r.data.location.country);
+          setTemp(r.data.forecast.forecastday[0].day.maxtemp_c);
+          setWeather(r.data.current.condition.text);
+          setIconImageUrl(r.data.current.condition.icon);
+        })
+        .catch((e) => {
+          console.log("Error: ", e);
+        });
     } catch (e) {
       console.log("error: ", e);
     }
   })();
 
-  axios
-    .get(`${baseUrl}`)
-    .then((r) => {
-      console.log(r.data);
-      setCity(r.data.location.name + ", " + r.data.location.country);
-      setTemp(r.data.forecast.forecastday[0].day.maxtemp_c);
-      setWeather(r.data.current.condition.text);
-      setIconImageUrl(r.data.current.condition.icon);
-    })
-    .catch((e) => {
-      console.log("Error: ", e);
-    });
-
   return (
     <View style={styles.container}>
       <View>
         <Text>{city}</Text>
+        <Text>{country}</Text>
       </View>
       <View>
         <Text>{temp} C</Text>
@@ -51,9 +53,9 @@ export default function Weather() {
       <View>
         <Image
           source={{
-            uri: "http://cdn.weatherapi.com/weather/64x64/day/113.png"
-            
-          }} style = {{ width: 100, height: 100 }}
+            uri: `http:${iconImageUrl}`,
+          }}
+          style={{ width: 100, height: 100, marginTop:5 }}
         />
       </View>
     </View>
--- tools/tools.js
+++ tools/tools.js
@@ -36,7 +36,7 @@ export async function getLocation() {
       granted = requestResponse.granted;
     }
     if (!granted) {
-      console.log("Location not authorizaed!");
+      //console.log("Location not authorizaed!");
       return;
     }
 
